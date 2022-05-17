e992436fa06f98f990fd9e34218e8292f4fac940
ishank2712 <91245869+ishank2712@users.noreply.github.com>
2022-05-17 23:54:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,48 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import Toast from 'react-native-toast-message';
+import { View, Text, StyleSheet,} from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './src/screens/HomeScreen';
+import NewScreen from './src/screens/NewScreen';
+import { Icon } from 'react-native-elements';
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#228CDB'
+            },
+                headerTintColor: '#fff'
+            }} 
+          initialRouteName="Home">
+        <Stack.Screen 
+    name="Home" 
+    component={HomeScreen} 
+    options={({navigation}) => ({
+        headerRight: () => (
+            <Icon 
+            name="plus" 
+            type="feather" 
+            color="#fff"
+            style={style.headerIcon}
+                                onPress={() => navigation.navigate('New')}
+    		/>
+    	)
+    })}
+/>
+        <Stack.Screen name="New" component={NewScreen} />
+      </Stack.Navigator>
+      <Toast />
+    </NavigationContainer>
+  );
+}
+const style = StyleSheet.create({
+  headerIcon: {
+    marginRight: 10
+  }
+});
